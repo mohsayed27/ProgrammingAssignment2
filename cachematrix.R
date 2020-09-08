0763c98fdf86bf3287a91ef50189b192f5573b58
mohsayed27 <mohsayed27@gmail.com>
2020-09-08 21:48:59 +0200

    updated functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## I have googled how to get inverse matrix in R
 ## then followed the example to change mean function by solve function
 
-
+## All is pretty simple and just like the example given
 makeCacheMatrix <- function(x = matrix()) {
 	inv <- NULL
 	set <- function(mat) {
@@ -17,17 +17,18 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## function solves for the cache by using the built-in solve function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         inv <- x$getinverse()
         if(!is.null(inv)) {
+				##gets into here if incerse is already calculated
                 message("getting cached data")
                 return(inv)
         }
-        data <- x$get()
-        inv <- solve(data) %*% data
-        x$setmean(inv)
-        inv		
+        data <- x$get() ##gets the matrix
+        inv <- solve(data,...) ##calculates the matrix inverse
+        x$setinverse(inv) 	## Stores Inverse ti be retrieved in the future
+        inv		 ##Return inverse matrix
 }
